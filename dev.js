d4e66e1a0b1cd7a9b7a0a43aeb84c25a91f6e34c
yeshaopeng@upg.cn <yeshaopeng@upg.cn>
2015-07-10 17:47:25 +0800

    c

--- dev.js
+++ dev.js
@@ -1,122 +1,173 @@
-(function() {	
-	var VERSION="1.0";
-	var guid = Date.now();
-	var log = {
-		realConsole: window.console,
-		wrapperMsg: function(msg) {
-			return typeof msg == "string" ? msg : JSON.stringify(msg);
-		},
-		init: function() {
-			window.console = {
-				log: function(msg) {
-					debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "log");
-					debug.consoleEntity.print();
-					debug.console.log(msg);
-				},
-				warn: function(msg) {
-					debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "warn");
-					debug.consoleEntity.print();
-					debug.console.warn(msg);
-				},
-				error: function(msg) {
-					debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "error");
-					debug.consoleEntity.print();
-					debug.console.error(msg);
-				},
-				dir: function(msg) {
-					debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "dir");
-					debug.consoleEntity.print();
-					debug.console.dir(msg);
-				},
-				info: function(msg) {
-					debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "info");
-					debug.consoleEntity.print();
-					debug.console.info(msg);
-				},
-				clear: function() {
-					debug.consoleEntity.clear(true);
-				},
-				time: function(msg) {
+(function() {
+		var VERSION = "1.0";
+		var guid = Date.now();
 
-				},
-				timeEnd: function(msg) {
+		var ID = {
+			DEVTool: "DEVTool" + guid,
+			menu: "menu" + guid,
+			toolbar: "toolbar" + guid,
+			Console: "Console" + guid,
+			msgShower: "msgShower" + guid,
+			edit: "edit" + guid
+		};
+		var CLASS_NAME = {
+			error: "error" + guid,
+			log: "log" + guid,
+			warn: "warn" + guid,
+			info: "info" + guid
+		}
 
-				},
-			};
-			//对原生的console对象进行重写
-		},
-		consoleEntity: {
-			//控制台
-			open: function() {
-				//显示控制台
-				this.screen.frame.style.display = "block";
-				this.screen.inputBox.focus();
+		var Log = {
+			realConsole: window.console,
+			wrapperMsg: function(msg) {
+				return typeof msg == "string" ? msg : JSON.stringify(msg);
 			},
-			close: function() {
-				//隐藏控制台
-				this.screen.frame.style.display = "none";
-
-			},
-			clear: function(clearStorage) {
-				//清除控制台内容
-				debug.consoleEntity.screen.msgBox.innerHTML = "";
-				if (clearStorage) {
-					debug.consoleEntity.storage.clear.call(debug.consoleEntity.storage);
-
-				}
-			},
-			print: function() {
-				var content = sessionStorage[this.storage.keyName];
-				content = content.split(this.storage.splitCode);
-				content = content.join("");
-				// content = "<p>" + content + "</p>";
-				this.screen.msgBox.innerHTML = content;
-
-			},
-			initScreen: function(style) {
-
-			},
-			screen: {
-				frame: {
-
-				},
-				msgBox: {
-
-				},
-				inputBox: {
-
-				}
-				//控制台输出输入屏幕就是一个dom节点，在调用initScreen之后正式有值
-			},
-			storage: {
-				//数据存储在sessionStorage或者localStorgae里面，以“”分隔每条数据
-				splitCode: "@$$@,",
-				keyName: "debuginfo",
-				swithTag: {
-					error: function(msg) {
-						return "<p style='color:red'>" + msg + "</p>"
-					},
+			init: function() {
+				window.console = {
 					log: function(msg) {
-						return "<p style='color:black'>" + msg + "</p>"
-					},
-					info: function(msg) {
-						return "<p style='color:#8eb9f5'>" + msg + "</p>"
+						debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "log");
+						debug.consoleEntity.print();
+						debug.console.log(msg);
 					},
 					warn: function(msg) {
-						return "<p style='color:rgb(255,255,0)'>" + msg + "</p>"
+						debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "warn");
+						debug.consoleEntity.print();
+						debug.console.warn(msg);
+					},
+					error: function(msg) {
+						debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "error");
+						debug.consoleEntity.print();
+						debug.console.error(msg);
+					},
+					dir: function(msg) {
+						debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "dir");
+						debug.consoleEntity.print();
+						debug.console.dir(msg);
+					},
+					info: function(msg) {
+						debug.consoleEntity.storage.push("&lt;&lt;" + debug.wrapperMsg(msg), "info");
+						debug.consoleEntity.print();
+						debug.console.info(msg);
+					},
+					clear: function() {
+						debug.consoleEntity.clear(true);
+					},
+					time: function(msg) {
+
+					},
+					timeEnd: function(msg) {
+
+					},
+				};
+				//对原生的console对象进行重写
+			},
+			consoleEntity: {
+				//控制台
+				open: function() {
+					//显示控制台
+					this.screen.frame.style.display = "block";
+					this.screen.inputBox.focus();
+				},
+				close: function() {
+					//隐藏控制台
+					this.screen.frame.style.display = "none";
+
+				},
+				clear: function() {
+					//清除控制台内容
+					debug.consoleEntity.screen.msgBox.innerHTML = "";
+					if (clearStorage) {
+						debug.consoleEntity.storage.clear.call(debug.consoleEntity.storage);
+
+					}
+				},
+				print: function() {
+					var content = sessionStorage[this.storage.keyName];
+					content = content.split(this.storage.splitCode);
+					content = content.join("");
+					// content = "<p>" + content + "</p>";
+					this.screen.msgBox.innerHTML = content;
+
+				},
+				initScreen: function(style) {
+
+				},
+				screen: {
+					frame: {
+
+					},
+					msgBox: {
+
+					},
+					inputBox: {
+
+					}
+				}
+				getCode: function(msg) {
+					msg = msg.replace(/<\/?[^>]+>/g, "");
+					msg = msg.replace(/&nbsp;?/g, "");
+					if (msg !== "") {
+						msg = "(" + msg + ")";
+					}
+					return msg;
+				},
+				exec: function(code) {
+					// 执行输入的js代码
+					try {
+						if (code !== "") {
+							var stemp = eval(code);
+							console.log(stemp);
+						}
+					} catch (e) {
+						console.error(e);
+					}
+				},
+				runListener: function(e) {
+					var keycode = e.keyCode;
+					var code = "";
+					if (keycode == 13) {
+						e.preventDefault();
+						debug.consoleEntity.storage.push("<div>&gt;&gt;" + debug.consoleEntity.screen.inputBox.value + "</div>");
+						code = debug.getCode(debug.consoleEntity.screen.inputBox.value);
+						debug.consoleEntity.screen.inputBox.value = "";
+						debug.run(code);
+					}
+
+				},
+				init: function(style) {
+					this.initConsole();
+					this.consoleEntity.initScreen(style);
+					this.consoleEntity.screen.inputBox.addEventListener("keypress", this.runListener, false);
+				}
+			}
+			var Storage = {
+				//数据存储在sessionStorage或者localStorgae里面，以“”分隔每条数据
+				splitCode: "@$$@",
+				keyName: "debuginfo" + guid,
+				swithTag: {
+					error: function(msg) {
+						return "<p " + CLASS_NAME.error + ">" + msg + "</p>";
+					},
+					log: function(msg) {
+						return "<p " + CLASS_NAME.log + ">" + msg + "</p>";
+					},
+					info: function(msg) {
+						return "<p " + CLASS_NAME.info + ">" + msg + "</p>";
+					},
+					warn: function(msg) {
+						return "<p " + CLASS_NAME.warn + ">" + msg + "</p>";
 					}
 				},
 				push: function(msg, tag) {
-					var _msg = msg;
 					if (tag && this.swithTag[tag]) {
-						_msg = this.swithTag[tag](_msg);
+						msg = this.swithTag[tag](msg);
 					} else {
-						_msg = this.swithTag["log"](_msg);
+						msg = this.swithTag["log"](msg);
 					}
 					try {
-						sessionStorage[this.keyName] += this.splitCode + _msg;
+						sessionStorage[this.keyName] += this.splitCode + msg;
 					} catch (e) {
-						sessionStorage[this.keyName] = _msg;
+						sessionStorage[this.keyName] = msg;
 					}
 
 				},
@@ -154,84 +205,33 @@
 						sessionStorage[this.keyName] = "";
 						return "";
 					}
+				},
+				clear: function() {
+					try {
+						sessionStorage[this.keyName] = "";
+					} catch (e) {
+						return;
+					}
 				}
-			},
-			unshift: function(msg) {
-				var _msg = msg;
-				if (tag) {
-					_msg = this.swithTag[tag]();
-				}
-				try {
-					sessionStorage[this.keyName] = _msg + this.splitCode + sessionStorage[this.keyName];
-				} catch (e) {
-					sessionStorage[this.keyName] = _msg;
-				}
-
-			},
-			clear: function() {
-				try {
-					sessionStorage[debug.consoleEntity.storage.keyName] = "";
-					debug.consoleEntity.screen.msgBox.innerHTML = "";
-				} catch (e) {
-					return;
-				}
-
-			}
-		},
-		getCode: function(msg) {
-			var _msg = msg.replace(/<\/?[^>]+>/g, "");
-			_msg = _msg.replace(/&nbsp;?/g, "");
-			if (msg !== "") {
-				_msg = "(" + _msg + ")";
-			}
-			return _msg;
-		},
-		exec: function(code) {
-			// 执行输入的js代码
-			try {
-				if (code !== "") {
-					var stemp = eval(code);
-					console.log(stemp);
-				}
-			} catch (e) {
-				console.error(e);
-			}
-		},
-		runListener: function(e) {
-			var keycode = e.keyCode;
-			var code = "";
-			if (keycode == 13) {
-				e.preventDefault();
-				debug.consoleEntity.storage.push("<div>&gt;&gt;" + debug.consoleEntity.screen.inputBox.value + "</div>");
-				code = debug.getCode(debug.consoleEntity.screen.inputBox.value);
-				debug.consoleEntity.screen.inputBox.value = "";
-				debug.run(code);
 			}
 
-		},
-		init: function(style) {
-			this.initConsole();
-			this.consoleEntity.initScreen(style);
-			this.consoleEntity.screen.inputBox.addEventListener("keypress", this.runListener, false);
-		}
-	}
+			function initScreen() {
 
-	function initScreen() {
+			}
 
-	}
-	function writeCSS(){
-		var styles=[];
-		styles.push();
-	}
+			function writeCSS() {
+				var styles = [];
+				styles.push();
+			}
 
-	var DEVTools = {
-		open: function() {
-			myConsole.consoleEntity.open();
-		},
-		close: function() {
-			myConsole.consoleEntity.close();
-		}
-	}
+			var DEVTools = {
+				open: function() {
+					myConsole.consoleEntity.open();
+				},
+				close: function() {
+					myConsole.consoleEntity.close();
+				}
+			}
 
-	window.DEVTools = DEVTools;
-})()
+			window.DEVTools = DEVTools;
+		})()
